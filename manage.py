953e7b1412b13e6e505475c2e111153f6541ebc7
abi-kgb <abinaya.kgb@gmail.com>
2026-07-23 15:34:41 +0530

    Make pymysql optional in manage.py and wsgi.py

--- manage.py
+++ manage.py
@@ -3,17 +3,21 @@
 import os
 import sys
 
-import pymysql
-pymysql.install_as_MySQLdb()
+try:
+    import pymysql
+    pymysql.install_as_MySQLdb()
 
-# Bypass strict database version checks for XAMPP's MariaDB version
-from django.db.backends.base.base import BaseDatabaseWrapper
-BaseDatabaseWrapper.check_database_version_supported = lambda self: None
+    # Bypass strict database version checks for XAMPP's MariaDB version
+    from django.db.backends.base.base import BaseDatabaseWrapper
+    BaseDatabaseWrapper.check_database_version_supported = lambda self: None
 
-# Disable RETURNING clause for MariaDB 10.4 compatibility
-from django.db.backends.mysql.features import DatabaseFeatures
-DatabaseFeatures.can_return_columns_from_insert = property(lambda self: False)
-DatabaseFeatures.can_return_rows_from_bulk_insert = property(lambda self: False)
+    # Disable RETURNING clause for MariaDB 10.4 compatibility
+    from django.db.backends.mysql.features import DatabaseFeatures
+    DatabaseFeatures.can_return_columns_from_insert = property(lambda self: False)
+    DatabaseFeatures.can_return_rows_from_bulk_insert = property(lambda self: False)
+except ImportError:
+    pass
+
 
 
 
--- myhrm/wsgi.py
+++ myhrm/wsgi.py
@@ -1,16 +1,20 @@
 import os
 
-import pymysql
-pymysql.install_as_MySQLdb()
+try:
+    import pymysql
+    pymysql.install_as_MySQLdb()
 
-# Bypass strict database version checks for XAMPP's MariaDB version
-from django.db.backends.base.base import BaseDatabaseWrapper
-BaseDatabaseWrapper.check_database_version_supported = lambda self: None
+    # Bypass strict database version checks for XAMPP's MariaDB version
+    from django.db.backends.base.base import BaseDatabaseWrapper
+    BaseDatabaseWrapper.check_database_version_supported = lambda self: None
 
-# Disable RETURNING clause for MariaDB 10.4 compatibility
-from django.db.backends.mysql.features import DatabaseFeatures
-DatabaseFeatures.can_return_columns_from_insert = property(lambda self: False)
-DatabaseFeatures.can_return_rows_from_bulk_insert = property(lambda self: False)
+    # Disable RETURNING clause for MariaDB 10.4 compatibility
+    from django.db.backends.mysql.features import DatabaseFeatures
+    DatabaseFeatures.can_return_columns_from_insert = property(lambda self: False)
+    DatabaseFeatures.can_return_rows_from_bulk_insert = property(lambda self: False)
+except ImportError:
+    pass
+
 
 
 
